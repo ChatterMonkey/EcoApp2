e7cf6ae0798dbb03939c8ca1b5093772621a465f
mayabasu <basu.maya@icloud.com>
2020-01-12 17:54:21 -0800

    Automated commit 2

--- backup_to_git.sh
+++ backup_to_git.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+cd /home/mayabasu/EcoApp_2
 git add -u
 git commit -m 'Automated commit 2'
 git push
--- runme.sh
+++ runme.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 while true; do
-    gunicorn app:app -b 0.0.0.0:4000 --log-file ecoapp2_logs.txt --log-level debug --capture-output
+    cd /home/mayabasu/EcoApp_2 && /usr/local/bin/gunicorn app:app -b 0.0.0.0:4000 --log-file ecoapp2_logs.txt --log-level debug --capture-output > /home/mayabasu/log1.txt 2>&1
+    sleep 5
 done
