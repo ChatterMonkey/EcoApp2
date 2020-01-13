789e805b7f26a1c67b193c68637563f04300991d
mayabasu <basu.maya@icloud.com>
2020-01-12 17:57:52 -0800

    Automated commit 2

--- runme.sh
+++ runme.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while true; do
-    cd /home/mayabasu/EcoApp_2 && /usr/local/bin/gunicorn app:app -b 0.0.0.0:4000 --log-file ecoapp2_logs.txt --log-level debug --capture-output > /home/mayabasu/log1.txt 2>&1
+    cd /home/mayabasu/EcoApp_2 && /usr/local/bin/gunicorn app:app -b 0.0.0.0:4000 --log-file /home/mayabasu/EcoApp_2/ecoapp2_logs.txt --log-level debug --capture-output > /home/mayabasu/log1.txt 2>&1
     sleep 5
 done
